f134665b7136e11e2bcc409154e079cd1956166d
Thirstypooch <juancocchi6@gmail.com>
2025-06-25 22:45:55 -0500

    debugging

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use App\Http\Controllers\FileEncryptionController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+
+Route::get('/view-log-500-server-error-xyz', function () {
+    $logPath = storage_path('logs/laravel.log');
+
+    if (!File::exists($logPath)) {
+        return 'Log file not found.';
+    }
+
+    // Set the content type to plain text and return the log content
+    return response(File::get($logPath), 200)
+        ->header('Content-Type', 'text/plain');
+});
 
 Route::get('/', function () {
     return view('welcome');
